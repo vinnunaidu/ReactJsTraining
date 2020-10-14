3a2f212ea486265888f6b549e94e85c5fb3b2b95
Vinayak <vnaidu.dp@gmail.com>
2020-10-14 16:57:31 +0100

    Implementing state

---
+++ Section5/seasons/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={lat:null, errorMessage:''};
+        window.navigator.geolocation.getCurrentPosition(
+            position=>{
+                this.setState({lat:position.coords.latitude});
+            },
+            err=>{
+                this.setState({errorMessage:err.message});
+            }
+
+        )
+    }
+
+    render(){
+        if(this.state.lat && !this.state.errorMessage){
+            return <div>Latitude:{this.state.lat}</div>
+        }
+
+        if(!this.state.lat && this.state.errorMessage){
+            return <div>Error:{this.state.errorMessage}</div>
+        }
+        return <div>Loading...</div>;
+    };
+};
+
+ReactDOM.render(<App/>, document.querySelector("#root"));
